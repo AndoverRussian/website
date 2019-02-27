10035c5ac497c42f364912fd8635aa209e21d874
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-27 21:25:52 +0000

    Deploy AndoverRussian/website to github.com/AndoverRussian/website.git:gh-pages

---
+++ README.md
@@ -0,0 +1,20 @@
+# Russian Course Materials
+This repository contains the Russian course materials initially designed for RUS200 at Phillips Academy. It was adapted from the previous non-digital course materials, which were themselves adapted from _Русский Язык Для Всех_. For more information, see [parussian.org](https://parussian.org).
+
+### Making Edits
+To edit the website generated from this code, simply edit files in this repository and save them to GitHub — the changes will automatically be pushed to parussian.org by TravisCI, a free continuous integration service for open source projects (like this website).
+
+### Structure
+The files that will be most useful are the lesson files in [`_lessons`](/_lessons). These are Markdown files, and can therefore also contain HTML.
+
+### Technologies
+This website uses the following technologies:
+
+* Jekyll (with a custom theme)
+* TravisCI
+
+As foundational components, it also uses:
+
+* GitHub
+* HTML, CSS, and JavaScript
+* Ruby
---
+++ assets/main.js
@@ -0,0 +1,63 @@
+window.onload = function () {
+    $("word").click(function () {
+        console.log("clicked")
+        var word = $(this).text();
+        window.open('https://en.wiktionary.org/wiki/' + word + "#Russian", '_blank');
+    });
+}
+
+function exerciseGiveAnswer(element) {
+    var input = $(element).prev();
+    var answer = input.attr("answer");
+    $(input).val(answer);
+    checkExercise(input);
+}
+
+function checkExercise(element) {
+    var input = $(element);
+    var button = input.next();
+    var val = input.val();
+    var answer = input.attr("answer");
+    if (val.toLowerCase() == answer.toLowerCase()) {
+        input.removeClass("is-invalid");
+        input.addClass("is-valid");
+        button.addClass("btn-success");
+        button.removeClass("btn-default")
+    } else {
+        input.removeClass("is-valid");
+        button.removeClass("btn-success");
+        button.addClass("btn-default");
+        input.addClass("is-invalid");
+    }
+}
+
+// From https://stackoverflow.com/questions/18999501/bootstrap-3-keep-selected-tab-on-page-refresh
+
+$(document).ready(function () {
+    if (location.hash) {
+        $("a[href='" + location.hash + "']").tab("show");
+    }
+    $(document.body).on("click", "a[data-toggle]", function (event) {
+        location.hash = this.getAttribute("href");
+    });
+});
+
+$(window).on("popstate", function () {
+    var anchor = location.hash || $("a[data-toggle='tab']").first().attr("href");
+    $("a[href='" + anchor + "']").tab("show");
+});
+
+// End from
+
+$('.map').mapster({
+    singleSelect: true,
+    fill : true,
+    fillOpacity : 0.6,
+    fillColor: 'blue',
+    onMouseover: function(e) {
+        $(this).mapster('set',false).mapster('set',true);
+    },
+    onMouseout: function(e) { 
+         $(this).mapster('set',false);
+    }
+});
